21eefd29d616a6eac1b153941dcbc3ed50829d66
meshramnaina123 <meshram.naina1@gmail.com>
2026-03-29 23:15:28 +0530

    New TestRepoProgram Added

---
+++ JavaTraining/src/operatorprograms/TestRepoProgram.java
@@ -0,0 +1,12 @@
+package operatorprograms;
+
+public class TestRepoProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first Java Program");
+		
+	}
+
+}
